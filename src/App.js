b4c8ec3699810fac6cd3391e227185ca6f42fe86
Sasirekha03 <108916484+Sasirekha03@users.noreply.github.com>
2023-03-19 15:52:39 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Team from "./Components/Team";
+
+function App() {
+  return (
+    <div className="App">
+      <Team/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Team.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class Team extends Component {
+  render() {
+    return (
+      <div>This is Team Functional Component</div>
+    )
+  }
+}
+
+export default Team
